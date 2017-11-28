23aab7608d4120943031676fb5fc723ec558462a
TheMaroco <maroco.miguel@gmail.com>
2017-11-28 20:42:04 +0000

    Update and rename Main to Main.py

---
+++ Main.py
@@ -0,0 +1,28 @@
+#####################################
+###########Projecto de Programação 1
+#### GRUPO 007
+#### Pedro Pereira ##
+#### Miguel Marôco fc51475
+#### Wu Zhen Zé ##
+#####################################
+
+AINDA A DECIDIR
+#####################################
+########### JOGO -- sim
+########### HINT -- sim
+########### REGRAS -- sim
+#####################################
+
+
+#Leitura dos dados do jogador
+nomeJogador = input("Nome:")  #Nome
+montante = float(input("Com quantas fichas vais iniciar o jogo?:")) #Dinheiro com que começa o jogo
+if type(montante) != float:   #Atribuição do valor "default" se o introduzido não for válido
+	montante = 100.0    
+aposta = float(input("Quanto apostas?:")    #Atribuição do valor "default" se o introduzido não for válido
+if type(aposta) != float:
+	aposta = 10.0
+
+
+
+###COMEÇA O JOGO###
