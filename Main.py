b24f1e1c051f976972bb18881bf7f28a03b74521
TheMaroco <maroco.miguel@gmail.com>
2017-11-28 21:02:52 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -21,13 +21,8 @@
 
 #Leitura dos dados do jogador
 nomeJogador = input("Nome:")  #Nome
-montante = float(input("Com quantas fichas vais iniciar o jogo?:")) #Dinheiro com que começa o jogo
-if type(montante) != float:   #Atribuição do valor "default" se o introduzido não for válido
-	montante = 100.0    
-aposta = float(input("Quanto apostas?:")    #Atribuição do valor "default" se o introduzido não for válido
-if type(aposta) != float:
-	aposta = 10.0
-
+montante = float(input("Com quantas fichas vais iniciar o jogo?:")) #Dinheiro com que começa o jogo   
+aposta = float(input("Quanto apostas?:")    #Valor de cada aposta!
 
 
 ###COMEÇA O JOGO###
