4e1ed4bae565fe132eeb8ff2ce25459b834a24fa
TheMaroco <maroco.miguel@gmail.com>
2017-12-09 17:53:22 +0000

    def ronda(i, jogador, aposta) 1.0
    -Adicionei a funçao ronda v.1.0
    -Adicionei uma funçaõ que mostra as cartas de acordo com a tipografia pedida.

--- Main.py
+++ Main.py
@@ -92,44 +92,146 @@ def ler_baralho(n):
     
     return listBaralho
 
+def mostra_mao(mao):
+    """Recebe a lista da mmão e devolve a string formatada dessa mesma mão
 
-"""def ronda(i,jogador,aposta):
+    Requires: list da mao
+    Ensures: Return da string obtida por formatação da lista
+    """
+    mao = str(mao)
+    mao = mao[1:(len(mao)-1)]
+    mao = mao.replace("'", "")
+    mao = mao.replace(" ", "")
+    mao = mao.replace("),(", ") (")
+    return mao
+
+def ronda(i,jogador,aposta):
     ganho = 0
     baralho = ler_baralho(i)
     mao_jogador = [baralho[0],baralho[2]]
     mao_dealer = [baralho[1],baralho[3]]
     valor_jogador = valor(mao_jogador) # pontos do jogador
     valor_dealer = valor(mao_dealer)   # pontos de dealer
+
+    print("*** Ronda", i, "***")
+    print("Dealer:", mostra_mao(mao_dealer)[:5], "(?,?)")
+    print("Jogador:", mostra_mao(mao_jogador), "-", valor_jogador,"-")
+    print("\n* Joga", jogador, "*")
     
     ###Joga o jogador:
-    if blackjack(mao_jogador):
+    if blackjack(mao_jogador):  #Caso de BlackJack na primeira cartada.
         resultado_jogador = 'vitória blackjack'
-    else:
+        print(jogador, "fez BLACKJACK!")
+    else: #Caso de não blackjack na primeira cartada.
         
-        decisao_jogador = lower(input("HIT, STAND ?"))
-        if decisao_jogador != 'hit' or decisao_jogador != 'stand':
+        decisao_jogador = (input("HIT, STAND ?")).lower()
+        print(decisao_jogador)
+        if decisao_jogador != 'hit' and decisao_jogador != 'stand': #Gestão de input inválido
             decisao_jogador = 'stand'
+
+
         
-        while  not bust(mao_jogador) and not blackjack(mao_jogador) and decisao_jogador == 'hit':
-            contador = 1
-            if lower(input("HIT, STAND ?")) ==  "hit":
-                mao_jogador.append(baralho[3+contador])
-            contador += 1
-            mao_jogador = valor(mao_jogador)   
-    
-    if blackjack(mao_jogador):
-        if blackjack(mao_dealer):
-            resultado = 'empate'
+            
+        #PRINT da opção do jogador
+        if decisao_jogador == "hit":
+            
+            while  (not bust(mao_jogador)) and (not blackjack(mao_jogador)) and decisao_jogador == "hit":
+                 print(jogador, "decidiu HIT")
+                 mao_jogador.append(baralho.pop(0))
+                 valor_jogador = valor(mao_jogador)
+                 print(mostra_mao(mao_jogador), "-", valor_jogador, "-")
+                 if not bust(mao_jogador) and not blackjack(mao_jogador):
+                     decisao_jogador = (input("HIT, STAND ?")).lower()
+
+            if decisao_jogador == "stand":
+                print(jogador, "decidiu STAND\n")
+                resultado_jogador = str(valor_jogador)
+                 
         else:
-            resultado = 'vitória blackjack'
-            ganho = (5 * aposta )/2
-    else:
-
-
-    ###Joga o dealer:
+            print(jogador, "decidiu STAND\n")
+            resultado_jogador = str(valor_jogador)
+     
+        
+        
        
 
-"""
+        if bust(mao_jogador):
+            resultado_jogador = "derrota"
+            resultado_dealer = "vitória"
+            resultado = "derrota"
+            ganho = -aposta
+            print("BUST com", valor_jogador, "pontos")
+        if blackjack(mao_jogador):
+            resultado_jogador = "vitória blackjack"
+            print(jogador, "fez BLACKJACK!")
+
+   
+     
+    ###Joga o dealer:
+            
+    if resultado_jogador == "vitória blackjack":
+        if valor(mao_dealer[0]) == 10 or valor(mao_dealer[0]) == 11:
+            if blackjack(mao_dealer):
+                resultado = "empate"
+            else:
+                resultado = "vitória blackjack"
+                ganho = (5*aposta)/2
+    if resultado_jogador != "derrota":
+        print("* Joga o dealer *")
+        print("Mão dealer:")
+        print(mostra_mao(mao_dealer), "-", valor_dealer, "-")
+        if blackjack(mao_dealer):
+            print("Dealer fez BLACKJACK!")
+            resultado_dealer = "vitória blackjack"
+        else:
+        
+            if valor_dealer < 17:
+                decisao_dealer = "hit"
+                print("Dealer decidiu HIT")
+            else:
+                decisao_dealer = "stand"
+                print("Dealer decidiu STAND")
+            while not blackjack(mao_dealer) and not bust(mao_dealer) and decisao_dealer == "hit":
+                mao_dealer.append(baralho.pop(0))
+                valor_dealer = valor(mao_dealer)
+                print(mostra_mao(mao_dealer), "-", valor_dealer, "-")
+                if valor_dealer < 17:
+                    decisao_dealer = "hit"
+                    print("Dealer decidiu HIT")
+                else:
+                    decisao_dealer = "stand"
+                    print("Dealer decidiu STAND")
+            resultado_dealer = str(valor_dealer)
+            if blackjack(mao_dealer):
+                print("Dealer fez BLACKJACK!")
+                resultado_dealer = "vitória blackjack"
+            if bust(mao_dealer):
+                resultado_dealer = "BUST"
+                print("BUST com", valor_dealer, "pontos")
+            
+
+    ###TESTAR O RESULTADO DA RONDA
+
+    if resultado_jogador == "vitória blackjack" and resultado_dealer == "vitória blackjack": # Empatam os dois com blackjack
+        resultado = "empate"
+        ganho = 0
+    elif resultado_dealer == "BUST": #Dealer perde por BUST
+        resultado = "vitória"
+        ganho = aposta
+    elif resultado_dealer < resultado_jogador: #Dealer perde por pontos
+        resultado = "vitória"
+        ganho = aposta
+    elif resultado_dealer > resultado_jogador: #Dealer ganha por pontos
+        resultado = "derrota"
+        ganho = -aposta
+    else:
+        resultado = "empate" #Empatam por pontos
+
+
+    return (resultado, ganho)
+
+
+
 
 #Leitura dos dados do jogador
 jogador = input("Nome: ")  #Nome
